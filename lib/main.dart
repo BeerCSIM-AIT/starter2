93cedefcbdb37e762bda09757362f9e6e6cbcfdb
BeerCSIM_AIT <st121032@ait.ac.th>
2021-07-11 11:29:01 +0700

    Add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:starter2/theme.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,9 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // final wordPair = WordPair.random();
+
     return MaterialApp(
       title: "Startup Namer",
       home: RandomWords(),
+      theme: ThemeData(primaryColor: primaryTheme),
     );
   }
 }
